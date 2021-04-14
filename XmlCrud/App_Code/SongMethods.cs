e3dd605184e57780f534c88c4910d41f5c7930ea
stefanpost268 <97079064@st.deltion.nl>
2021-04-14 14:59:15 +0200

    eerste versie

---
+++ XmlCrud/App_Code/SongMethods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace XmlCrud.App_Code
+{
+    public class SongMethods
+    {
+        private DataSet ds = new DataSet("playlist");
+
+        
+
+        public DataSet GetAllSongs(string File)
+        {
+            
+            DataTable dtSong = new DataTable("song");
+
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcTitle = new DataColumn("title");
+            DataColumn dcArtist = new DataColumn("artist");
+            DataColumn dcYear = new DataColumn("year");
+            DataColumn dcTime = new DataColumn("time");
+            DataColumn dcGenre = new DataColumn("genre");
+            DataColumn dcFile = new DataColumn("file");
+
+            dtSong.Columns.Add(dcId);
+            dtSong.Columns.Add(dcTitle);
+            dtSong.Columns.Add(dcArtist);
+            dtSong.Columns.Add(dcYear);
+            dtSong.Columns.Add(dcTime);
+            dtSong.Columns.Add(dcFile);
+
+            ds.ReadXml(HttpContext.Current.Server.MapPath("App_Data/Playlist.xml"));
+            return ds;
+        }
+        public DataRow GetEmtyDataRow()
+        {
+            DataRow dr = ds.Tables["song"].NewRow();
+            return dr;
+        }
+
+        public void CreateSong(DataRow dataRow, string file)
+        {
+            ds.Tables["song"].Rows.Add(dataRow);
+            ds.WriteXml(HttpContext.Current.Server.MapPath(file));
+        }
+
+    }
+    
+}
+
